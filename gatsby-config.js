ae7fbf862eef47960ee0f71ad7e4f564659fa0bd
Nathan Tredgett <nathantredgett@gmail.com>
2020-07-16 22:10:46 +0100

    added basic logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `BKMRKD.`,
-    description: `Bookmarks BKMRKD.`,
+    title: `BKMRK`,
+    description: `Bookmarks BKMRK'D.`,
     author: `@nathan-kansu`,
   },
   plugins: [
@@ -43,7 +43,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
