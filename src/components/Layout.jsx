e516e8cba8523784164261e8895e51ea812e6fbb
Nathan Tredgett <nathantredgett@gmail.com>
2020-07-17 21:57:37 +0100

    reverted Layout graphql query

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,27 +7,27 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-// import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 
-// import Header from "./Header"
+import Header from "./Header"
 import Main from "./Main"
 import GlobalStyle from "../styles/global"
 
 const Layout = ({ children }) => {
-  //   const data = useStaticQuery(graphql`
-  //     query SiteTitleQuery {
-  //       site {
-  //         siteMetadata {
-  //           title
-  //         }
-  //       }
-  //     }
-  //   `)
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
 
   return (
     <>
       <GlobalStyle />
-      {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
+      <Header siteTitle={data.site.siteMetadata.title} />
       <Main>{children}</Main>
     </>
   )
