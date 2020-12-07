d5a2100e82a020217f537236af5d8f52e7857eb4
Nathan Tredgett <nathantredgett@gmail.com>
2020-12-07 09:48:57 +0000

    Layout linting

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -9,21 +9,10 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./Header"
 import Main from "./Main"
 import GlobalStyle from "../styles/global"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
       <GlobalStyle />
